a9d1d3ec3416ec318fb90dced6ad0a96c594079f
Ayush-Walia <ayushwalia999@gmail.com>
2020-10-03 06:03:49 +0000

    Revert "VolumeDialog: Hide the music row if there is an ally stream"
    This reverts commit f26375fc7ac282662e27ebf7aad620cd8a201b23.

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -168,10 +168,8 @@ public class VolumeDialogImpl implements VolumeDialog,
     private boolean mShowA11yStream;
 
     private int mActiveStream;
-    private int mAllyStream;
     private int mPrevActiveStream;
     private boolean mAutomute = VolumePrefs.DEFAULT_ENABLE_AUTOMUTE;
-    private boolean mMusicHidden;
     private boolean mSilentMode = VolumePrefs.DEFAULT_ENABLE_SILENT_MODE;
     private State mState;
     private SafetyWarningDialog mSafetyWarning;
@@ -355,9 +353,6 @@ public class VolumeDialogImpl implements VolumeDialog,
         initRingerH();
         initSettingsH();
         initODICaptionsH();
-
-        mAllyStream = -1;
-        mMusicHidden = false;
     }
 
     private final OnComputeInternalInsetsListener mInsetsListener = internalInsetsInfo -> {
@@ -574,10 +569,6 @@ public class VolumeDialogImpl implements VolumeDialog,
             });
         }
 
-        if (mAllyStream == -1) {
-            mAllyStream = mActiveStream;
-        }
-
         if (mExpandRowsView != null) {
             mExpandRowsView.setVisibility(
                     mDeviceProvisionedController.isCurrentUserSetup() &&
@@ -587,10 +578,6 @@ public class VolumeDialogImpl implements VolumeDialog,
         if (mExpandRows != null) {
             mExpandRows.setOnClickListener(v -> {
                 rescheduleTimeoutH();
-                if (mMusicHidden) {
-                    Util.setVisOrGone(findRow(AudioManager.STREAM_MUSIC).view,
-                            !mExpanded);
-                }
                 Util.setVisOrGone(findRow(AudioManager.STREAM_RING).view, !mExpanded);
                 Util.setVisOrGone(findRow(STREAM_ALARM).view, !mExpanded);
                 if (!isNotificationVolumeLinked()) {
@@ -598,7 +585,7 @@ public class VolumeDialogImpl implements VolumeDialog,
                             findRow(AudioManager.STREAM_NOTIFICATION).view, !mExpanded);
                 }
 
-                if (mExpanded) mController.setActiveStream(mAllyStream);
+                if (mExpanded) mController.setActiveStream(AudioManager.STREAM_MUSIC);
                 mExpandRows.setExpanded(!mExpanded);
                 mExpanded = !mExpanded;
             });
@@ -920,8 +907,6 @@ public class VolumeDialogImpl implements VolumeDialog,
                     }
                     mExpanded = false;
                     mExpandRows.setExpanded(mExpanded);
-                    mAllyStream = -1;
-                    mMusicHidden = false;
                     tryToRemoveCaptionsTooltip();
                     mController.notifyVisible(false);
                 }, 50));
@@ -943,12 +928,6 @@ public class VolumeDialogImpl implements VolumeDialog,
 
     private boolean shouldBeVisibleH(VolumeRow row, VolumeRow activeRow) {
         boolean isActive = row.stream == activeRow.stream;
-
-        if (row.stream == AudioSystem.STREAM_MUSIC &&
-                activeRow.stream != AudioSystem.STREAM_MUSIC && !mExpanded) {
-            mMusicHidden = true;
-            return false;
-        }
 
         if (isActive) {
             return true;
